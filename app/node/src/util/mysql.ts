5ee134f7ba48f22bed1353616c4a1f9e6fcae676
88-yana <85784752+88-yana@users.noreply.github.com>
2023-06-17 21:05:51 +0900

    change pool

--- app/node/src/util/mysql.ts
+++ app/node/src/util/mysql.ts
@@ -1,53 +1,53 @@
-import { createPool, Pool, PoolOptions } from "mysql2/promise";
+// import { createPool, Pool, PoolOptions } from "mysql2/promise";
 
-const mysqlOption: PoolOptions = {
-  host: "mysql",
-  user: "mysql",
-  password: "mysql",
-  database: "app",
-  waitForConnections: true,
-  connectionLimit: 10,
-};
+// const mysqlOption: PoolOptions = {
+//   host: "mysql",
+//   user: "mysql",
+//   password: "mysql",
+//   database: "app",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// };
 
-const pool: Pool = createPool(mysqlOption);
+// const pool: Pool = createPool(mysqlOption);
 
-export default pool;
+// export default pool;
 
 
 //下と上であまり変わらなかった。
 
-// import { createPool, Pool, PoolOptions } from "mysql2/promise";
+import { createPool, Pool, PoolOptions } from "mysql2/promise";
 
-// class Database {
-//   private static instance: Database;
-//   private pool: Pool;
+class Database {
+  private static instance: Database;
+  private pool: Pool;
 
-//   private constructor() {
-//     const mysqlOptions: PoolOptions = {
-//       host: "mysql",
-//       user: "mysql",
-//       password: "mysql",
-//       database: "app",
-//       waitForConnections: true,
-//       connectionLimit: 30,
-//     };
+  private constructor() {
+    const mysqlOptions: PoolOptions = {
+      host: "mysql",
+      user: "mysql",
+      password: "mysql",
+      database: "app",
+      waitForConnections: true,
+      connectionLimit: 30,
+    };
 
-//     this.pool = createPool(mysqlOptions);
-//   }
+    this.pool = createPool(mysqlOptions);
+  }
 
-//   public static getInstance(): Database {
-//     if (!Database.instance) {
-//       Database.instance = new Database();
-//     }
-//     return Database.instance;
-//   }
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
 
-//   public getPool(): Pool {
-//     return this.pool;
-//   }
-// }
+  public getPool(): Pool {
+    return this.pool;
+  }
+}
 
-// const database = Database.getInstance();
-// const pool = database.getPool();
+const database = Database.getInstance();
+const pool = database.getPool();
 
-// export default pool;
+export default pool;
