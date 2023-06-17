6d36d45420e6fffbe009a864e3af47dd21a9262e
88-yana <85784752+88-yana@users.noreply.github.com>
2023-06-17 22:12:05 +0900

    ADD:create for match

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -2,6 +2,9 @@ USE app;
 CREATE INDEX idx_user_mail_password ON user (mail, password);
 CREATE INDEX idx_entry_date ON user (entry_date);
 CREATE INDEX idx_kana ON user (kana);
+CREATE INDEX idx_user_id ON match_group_member (user_id);
 
 ALTER TABLE user ADD FULLTEXT INDEX user_full_idx (user_name) WITH PARSER ngram;
 ALTER TABLE user ADD FULLTEXT INDEX kana_full_idx (kana) WITH PARSER ngram;
+
+
